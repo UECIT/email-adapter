b209e2b89a694b51e0491c120cf91ca75e1b0df9
Sagar P Ghagare <Sagar13387@Mastek.com>
2020-06-19 12:55:02 +0100

    calibri font

--- src/main/java/uk/nhs/digital/iucds/middleware/transformer/PDFTransformer.java
+++ src/main/java/uk/nhs/digital/iucds/middleware/transformer/PDFTransformer.java
@@ -6,6 +6,8 @@ import com.amazonaws.util.StringInputStream;
 import com.itextpdf.html2pdf.ConverterProperties;
 import com.itextpdf.html2pdf.HtmlConverter;
 import com.itextpdf.html2pdf.resolver.font.DefaultFontProvider;
+import com.itextpdf.io.font.FontProgram;
+import com.itextpdf.io.font.FontProgramFactory;
 import com.itextpdf.io.source.ByteArrayOutputStream;
 import com.itextpdf.layout.font.FontProvider;
 import lombok.Data;
@@ -17,14 +19,24 @@ import lombok.Data;
 @Component
 public class PDFTransformer {
 
-  private static final String FONTS = "src/main/resources/fonts/calibri/";
+  public static final String[] FONTS = {
+      "src/main/resources/fonts/calibri/CALIBRI.TTF",
+      "src/main/resources/fonts/calibri/CALIBRIB.TTF",
+      "src/main/resources/fonts/calibri/CALIBRII.TTF",
+      "src/main/resources/fonts/calibri/CALIBRIL.TTF",
+      "src/main/resources/fonts/calibri/CALIBRILI.TTF",
+      "src/main/resources/fonts/calibri/CALIBRIZ.TTF"
+  };
   
   public byte[] transform(String html) throws IOException {
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
     ConverterProperties properties = new ConverterProperties();
-    FontProvider fontProvider = new DefaultFontProvider();
-    fontProvider.addDirectory(FONTS);
+    FontProvider fontProvider = new DefaultFontProvider(false, false, false);
+    for (String font : FONTS) {
+        FontProgram fontProgram = FontProgramFactory.createFont(font);
+        fontProvider.addFont(fontProgram);
+    }
     properties.setFontProvider(fontProvider);
     HtmlConverter.convertToPdf(new StringInputStream(html), outputStream, properties);
 
