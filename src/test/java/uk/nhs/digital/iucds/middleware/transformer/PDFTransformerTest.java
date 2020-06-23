698dc482e7d7431d32061eb8f5a0229bcff373f3
Sagar P Ghagare <Sagar13387@Mastek.com>
2020-06-23 19:31:04 +0100

    unit tests

--- src/test/java/uk/nhs/digital/iucds/middleware/transformer/PDFTransformerTest.java
+++ src/test/java/uk/nhs/digital/iucds/middleware/transformer/PDFTransformerTest.java
@@ -4,8 +4,11 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import org.apache.commons.io.FileUtils;
+import org.jsoup.Jsoup;
 import org.junit.Before;
 import org.junit.Test;
+import uk.nhs.digital.iucds.middleware.NHS111ReportData;
+import uk.nhs.digital.iucds.middleware.service.NHS111ReportDataBuilder;
 
 public class PDFTransformerTest {
 
@@ -13,15 +16,22 @@ public class PDFTransformerTest {
   public static final String HTML_FILE = "src/test/resources/input.html";
 
   private PDFTransformer pdfTransformer;
+  private HTMLReportTransformer htmlReportTransformer;
 
   @Before
   public void setup() {
     pdfTransformer = new PDFTransformer();
+    htmlReportTransformer = new HTMLReportTransformer();
   }
 
   @Test
   public void transformEmptyReportPdf() throws IOException {
-    byte[] pdfData = pdfTransformer.transform(getInputHtml());
+    NHS111ReportDataBuilder build = new NHS111ReportDataBuilder();
+    NHS111ReportData buildNhs111Report = build.buildNhs111Report(Jsoup.parse(getInputHtml()));
+
+    String nhs111ReportString = htmlReportTransformer.transform(buildNhs111Report);
+    
+    byte[] pdfData = pdfTransformer.transform(nhs111ReportString);
 
     FileUtils.writeByteArrayToFile(new File(OUTPUT_PATH), pdfData);
   }
